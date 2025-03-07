ec28c54de83a2fa35659059a404d9b26ccdb3b03
doom <62922003+DOOM-EXE@users.noreply.github.com>
2025-03-07 02:34:40 -0400

    creacion de login y conexion a la bd

---
+++ config/conexion.php
@@ -0,0 +1,17 @@
+<?php 
+
+$host = "localhost"; // Cambia si tu servidor es diferente
+$dbname = "medicalweb"; // Reemplaza con el nombre real de tu base de datos
+$username = "root"; // Usuario de tu base de datos
+$password = ""; // Contraseña de la base de datos
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //echo "✅ Conexión exitosa a la base de datos.";
+} catch (PDOException $e) {
+    echo "❌ Error en la conexión: " . $e->getMessage();
+}
+
+
+?>
