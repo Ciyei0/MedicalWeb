19d63c8dc4db8b268aebf1c59f90038420c74ee2
Ciyei0 <raymondisaac97@gmail.com>
2025-03-10 13:46:19 -0400

    Plantilla panel de control

--- templates/footer.php
+++ templates/footer.php
@@ -2,7 +2,7 @@
     </div> <!-- Cierre de content -->
 
     <footer class="footer">
-        <p>MedicalWeb </p>
+        <p>MedicalWeb</p>
     </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
