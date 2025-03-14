d15675d99d38bb28ef75db5556b28d06f55ee808
Ciyei0 <raymondisaac97@gmail.com>
2025-03-14 12:46:16 -0400

    Eliminacion de carpeta innecesarias

--- vistas/admin/controlpanel.php
+++
@@ -1,8 +0,0 @@
-<?php include '../../templates/header.php'; ?>
-
-
-<h1>Bienvenido a MedicalWeb</h1>
-<p>Aquí puedes gestionar las citas y agenda</p>
-
-
-<?php include '../../templates/footer.php'; ?>
